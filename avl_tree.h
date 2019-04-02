ee6cb480efd9fa3a64b486c63283127168a9b14f
Marcel Plakolb <marcel.plakolb@liwest.at>
2019-04-02 11:39:07 +0200

    uwu

--- avl_tree.h
+++ avl_tree.h
@@ -2,21 +2,11 @@
 #define BALANCEDTREE_AVL_TREE_H
 
 #include "node.h"
-
-/**
- * Left rotates the subtree rooted with the given node
- * @param node The root of the subtree
- * @return The new root of the subtree
- */
-Node rotate_left(Node node);
-
-/**
- * Inserts a new node (with the given key) in the subtree rooted with the given node
- * @param node The root of the subtree
- * @param key The key of the new node
- * @return Pointer to the (new) root node
- * @note This is a recursive function
- */
-Node insert(Node node, int key);
-
+int 	max (int n1, int n2);
+Node 	rotate_right (Node node);
+Node 	rotate_left (Node node);
+int 	get_balance (Node node);
+Node 	insert (Node node, int key);
+void 	print_postorder (Node node);
+void 	unbalanced_insert (Node root, int key);
 #endif //BALANCEDTREE_AVL_TREE_H
---
+++ node.cpp
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "node.h"
+
+struct NodeImplementation{
+  int element;
+  Node key;
+  Node right;
+  Node left;
+};
+
+int 	get_key (Node node){
+  return node->key->element;
+}
+
+Node 	get_left (Node node){
+  return node->left;
+}
+
+void 	set_left (Node node, Node left_node){
+  node->left=leftNode;
+}
+
+Node 	get_right (Node node){
+  return node->right;
+}
+
+void 	set_right (Node node, Node right_node){
+  node->right=right_node;
+}
+
+int 	get_height (Node node){
+return 0;
+}
+
+void 	set_height (Node node, int height){
+
+}
+
+Node 	create_node (int key){
+  Node newNode=(Node)malloc(sizeof(struct NodeImplementation));
+  newNode->key->element=key;
+  newNode->left=0;
+  newNode->right=0;
+}
