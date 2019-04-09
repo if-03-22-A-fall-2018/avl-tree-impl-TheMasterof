4931a7fc812b14e6627608c8834c322c3f6a7987
Marcel Plakolb <marcel.plakolb@liwest.at>
2019-04-09 11:09:06 +0200

    still wip

--- avl_tree.h
+++ avl_tree.h
@@ -2,11 +2,31 @@
 #define BALANCEDTREE_AVL_TREE_H
 
 #include "node.h"
-int 	max (int n1, int n2);
-Node 	rotate_right (Node node);
-Node 	rotate_left (Node node);
+
+/**
+ * Left rotates the subtree rooted with the given node
+ * @param node The root of the subtree
+ * @return The new root of the subtree
+ */
+Node rotate_left(Node node);
+
+Node rotate_right(Node node);
+
 int 	get_balance (Node node);
-Node 	insert (Node node, int key);
+
 void 	print_postorder (Node node);
-void 	unbalanced_insert (Node root, int key);
+
+Node 	unbalanced_insert (Node root, int key);
+
+int max (int n1, int n2);
+
+/**
+ * Inserts a new node (with the given key) in the subtree rooted with the given node
+ * @param node The root of the subtree
+ * @param key The key of the new node
+ * @return Pointer to the (new) root node
+ * @note This is a recursive function
+ */
+Node insert(Node node, int key);
+
 #endif //BALANCEDTREE_AVL_TREE_H
--- node.cpp
+++ node.cpp
@@ -1,44 +1,67 @@
-#include <stdlib.h>
 #include "node.h"
+#include <stdlib.h>
 
-struct NodeImplementation{
-  int element;
-  Node key;
-  Node right;
-  Node left;
-};
-
-int 	get_key (Node node){
-  return node->key->element;
+int get_key(Node node) {
+  if(node==0)
+  {
+    return 0;
+  }
+  return node->key;
 }
 
-Node 	get_left (Node node){
+Node get_left(Node node) {
+  if(node==0)
+  {
+    return 0;
+  }
   return node->left;
 }
 
-void 	set_left (Node node, Node left_node){
-  node->left= left_node;
+void set_left(Node node, Node left_node) {
+  if(node==0)
+  {
+    return;
+  }
+  node->left = left_node;
 }
 
-Node 	get_right (Node node){
+Node get_right(Node node) {
+  if(node==0)
+  {
+    return 0;
+  }
   return node->right;
 }
 
-void 	set_right (Node node, Node right_node){
-  node->right=right_node;
+void set_right(Node node, Node right_node) {
+  if(node==0)
+  {
+    return;
+  }
+  node->right = right_node;
 }
 
-int 	get_height (Node node){
-return 0;
+int get_height(Node node) {
+  if(node==0)
+  {
+    return 0;
+  }
+  return node->height;
 }
 
-void 	set_height (Node node, int height){
-
+void set_height(Node node, int height) {
+  if(node==0)
+  {
+    return;
+  }
+  node->height = height;
 }
 
-Node 	create_node (int key){
-  Node newNode=(Node)malloc(sizeof(struct NodeImplementation));
-  newNode->key->element=key;
-  newNode->left=0;
-  newNode->right=0;
+Node create_node(int key) {
+  Node new_node = (Node)malloc(sizeof(struct NodeImplementation));
+  new_node->right = 0;
+  new_node->left = 0;
+  new_node->key = key;
+  new_node->height = 1;
+  return new_node;
 }
